02f0d8f78181cbddb81856cdf6b38ae973eb6057
Ole Strohm <strohm99@gmail.com>
2017-10-08 02:19:36 +0200

    soon to be optimized

--- image.h
+++ image.h
@@ -11,65 +11,73 @@
 
 #define LOG(x) (std::cout << x << std::endl)
 
-#define IMAGE_FORMAT_RGBA 6
+#ifdef VIVID_OPENGL
+#define VIVID_IMAGE_FORMAT_RGB 0x1907
+#define VIVID_IMAGE_FORMAT_RGBA 0x1908
+#else
+#define VIVID_IMAGE_FORMAT_RGB 3
+#define VIVID_IMAGE_FORMAT_RGBA 4
+#endif
 
-struct Chunk {
-	unsigned int length;
-	std::string type;
-	std::vector<unsigned char> data;
-	unsigned int crc;
+namespace vivid { namespace util {
 	
-	Chunk(unsigned int length, std::string type, std::vector<unsigned char>& data, unsigned int crc)
-			: length(length), type(std::move(type)), data(data), crc(crc) {}
-};
-
-struct Pixel {
-	union {
-		struct {
-			unsigned char red, green, blue, alpha;
-		};
-		struct {
-			unsigned char r{}, g{}, b{}, a{};
-		};
+	struct Chunk {
+		unsigned int length;
+		std::string type;
+		std::vector<unsigned char> data;
+		unsigned int crc;
+		
+		Chunk(unsigned int length, std::string type, std::vector<unsigned char>& data, unsigned int crc)
+				: length(length), type(std::move(type)), data(data), crc(crc) {}
 	};
 	
-	Pixel(unsigned char alpha, unsigned char red, unsigned char green, unsigned char blue)
-			: alpha(alpha), red(red), green(green), blue(blue) {}
-	Pixel()
-			: alpha(0), red(0), green(0), blue(0) {}
+	struct Pixel {
+		union {
+			struct {
+				unsigned char red, green, blue, alpha;
+			};
+			struct {
+				unsigned char r{}, g{}, b{}, a{};
+			};
+		};
+		
+		Pixel(unsigned char alpha, unsigned char red, unsigned char green, unsigned char blue)
+				: alpha(alpha), red(red), green(green), blue(blue) {}
+		Pixel()
+				: alpha(0), red(0), green(0), blue(0) {}
+		
+		unsigned int color() const { return a << 24 | r << 16 | g << 8 | b; }
+	};
 	
-	unsigned int color() { return a << 24 | r << 16 | g << 8 | b; }
-};
-
-struct ImageFormat {
-	unsigned int width;
-	unsigned int height;
-	unsigned char bitDepth;
-	unsigned char colorFormat;
+	struct ImageFormat {
+		unsigned int width;
+		unsigned int height;
+		unsigned char bitDepth;
+		unsigned short colorFormat;
+		
+		ImageFormat()
+				: width(0), height(0), bitDepth(0), colorFormat(0) {}
+	};
 	
-	ImageFormat()
-			: width(0), height(0), bitDepth(0), colorFormat(0) {}
-};
-
-class Image {
-private:
-	ImageFormat format;
-	Pixel* pixels;
-public:
-	explicit Image(const std::string& path);
-	~Image();
+	class Image {
+	private:
+		ImageFormat format;
+		Pixel* pixels;
+	public:
+		explicit Image(const std::string& path);
+		~Image();
+		
+		inline const Pixel& getPixel(unsigned int& x, unsigned int& y) { return pixels[x + y * format.width]; }
+		inline const Pixel* const getPixels() const { return pixels; }
+		inline const unsigned int& getWidth() const { return format.width; }
+		inline const unsigned int& getHeight() const { return format.height; }
+		inline const unsigned char& getBitDepth() const { return format.bitDepth; }
+		inline const unsigned short& getColorFormat() const { return format.colorFormat; }
+		
+		inline const ImageFormat& getFormat() const { return format; }
+	private:
+		void loadChunks(std::vector<Chunk>& chunks, const unsigned char* data, unsigned int size);
+		Chunk loadChunk(const unsigned char* data, unsigned int& offset);
+	};
 	
-	inline const Pixel& getPixel(unsigned int& x, unsigned int& y) { return pixels[x + y * format.width]; }
-	inline const Pixel* const getPixels() const { return pixels; }
-	inline const unsigned int& getWidth() const { return format.width; }
-	inline const unsigned int& getHeight() const { return format.height; }
-	inline const unsigned char& getBitDepth() const { return format.bitDepth; }
-	inline const unsigned char& getColorFormat() const { return format.colorFormat; }
-	
-	inline const ImageFormat& getFormat() const { return format; }
-private:
-	void loadChunks(std::vector<Chunk>& chunks, const unsigned char* data, unsigned int size);
-	Chunk loadChunk(const unsigned char* data, unsigned int& offset);
-};
-
-
+}}
--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include "image.h"
 
 int main() {
-	Image image("squiggles.png");
+	using namespace vivid;
+	using namespace util;
+	
+	Image image("cartoon_goat.png");
 	LOG("Size: " << image.getWidth() << "x" << image.getHeight());
 	const Pixel* pixels = image.getPixels();
 
-	int x = 21;
-	int y = 5;
+	int x = 0;
+	int y = 0;
 	Pixel pixel = pixels[x + y * image.getWidth()];
 	std::printf("%08X\n", pixel.color());
 	
